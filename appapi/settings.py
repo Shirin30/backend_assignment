f0ce7679a7259af80cfd4502f131d615824fbec9
shirin <shirin.jain@iitg.ac.in>
2022-01-15 16:36:52 +0530

    new commit

--- appapi/settings.py
+++ appapi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 import datetime
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -166,8 +167,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
+django_heroku.settings(locals())
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
